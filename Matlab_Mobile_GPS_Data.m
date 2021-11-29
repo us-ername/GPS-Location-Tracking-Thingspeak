fa561bdbe69b60300749aaa5ee6c4eb595d6cba9
us-ername <50411305+us-ername@users.noreply.github.com>
2021-11-29 14:35:12 -0500

    Update and rename main.m to Matlab_Mobile_GPS_Data.m

---
+++ Matlab_Mobile_GPS_Data.m
@@ -0,0 +1,17 @@
+% m = mobiledev; 
+m.PositionSensorEnabled = 1; 
+m.Logging = 1; %required to send to mobiledev, device sends data to matlab 
+while (m.Logging == 1) 
+  for x = 1:15 
+      pause(1); 
+  end 
+  disp(m); 
+  %[lat,lon] = poslog(m); 
+  lat = m.latitude; 
+  lon= m.longitude; 
+
+  disp(lat); 
+  disp(lon); 
+
+  thingSpeakWrite(1588067,[lat, lon],'WriteKey','2U35DEMER57BQD1I'); 
+end 
